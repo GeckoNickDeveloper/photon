163af89f14041915d72f14032cc268062915c81e
GeckoNickDeveloper <nicholas.nick.kania.development@gmail.com>
2023-12-15 12:01:11 +0100

    Software Update
    > Added 'History' page

---
+++ lib/pages/history_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:photon/providers/providers.dart';
+
+class HistoryPage extends StatelessWidget {
+  final String serverInfos;
+  
+  const HistoryPage({super.key, required this.serverInfos});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Upload'),
+      ),
+      body: Center(
+        child: Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Placeholder Text',
+              ),
+              Text(serverInfos),
+              Consumer(
+                builder:(context, ref, child) {
+                  return ListView.builder(
+                    itemCount: ref.watch(imageListNotifier).length,
+                    itemBuilder:(context, index) {
+                      return ListTile(
+                        title: Text(
+                          ref.watch(imageListNotifier)[index].name
+                        )
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          // TODO: manage cancel
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.backpack),
+      ),
+    );
+  }
+}
