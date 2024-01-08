2ccdeed076b871f3b9c9a5c2f331834ec4f3e79f
GeckoNickDeveloper <nicholas.nick.kania.development@gmail.com>
2024-01-08 22:03:54 +0100

    Software Update
    > Added timeout handling in requests

--- lib/pages/history/screen/error_screen.dart
+++ lib/pages/history/screen/error_screen.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:photon/exception/photon_bad_request_exception.dart';
-import 'package:photon/exception/photon_base_exception.dart';
-import 'package:photon/exception/photon_conflict_exception.dart';
 import 'package:photon/exception/photon_forbidden_exception.dart';
-import 'package:photon/exception/photon_not_implemented_exception.dart';
-import 'package:photon/exception/photon_teapot_exception.dart';
-import 'package:photon/exception/photon_unknown_status_exception.dart';
+import 'package:photon/lib.dart';
 import 'package:photon/widgets/back_home_elevated_button.dart';
 
 class ErrorScreen extends StatelessWidget {
@@ -16,27 +10,7 @@ class ErrorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String text = '';
-    
-    if (error is PhotonBaseException) {
-      if (error is PhotonBadRequestException) {
-        text = 'Bad Request';
-      } else if (error is PhotonForbiddenException) {
-        text = 'Forbidden';
-      } else if (error is PhotonConflictException) {
-        text = 'Conflict';
-      } else if (error is PhotonTeapotException) {
-        text = 'Teapot';
-      } else if (error is PhotonNotImplementedException) {
-        text = 'Not Implemented';
-      } else if (error is PhotonUnknownStatusException) {
-        text = 'Unknown Status';
-      }
-    } else if (error is ClientException) {
-      text = 'An error occured with the client. Check network connectivity and be assured to be on the same network.';
-    } else {
-      text = 'Unknown Exception';
-    }
+    String text = getCustomMessageFromError(error);
     
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
